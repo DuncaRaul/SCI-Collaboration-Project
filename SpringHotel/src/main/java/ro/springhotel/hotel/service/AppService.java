6476ff4e9a5e91136523b5a07a280dcc34771243
andrei <andrei_ciliboae@yahoo.com>
2017-12-13 15:13:46 +0200

    Create Admin, Client, App, Exception Service class and java doc for the classes

---
+++ SpringHotel/src/main/java/ro/springhotel/hotel/service/AppService.java
@@ -0,0 +1,11 @@
+package ro.springhotel.hotel.service;
+
+/**
+ * AppService is the class that holds all the common services used by the Admin and the User
+ * and methods specific to the Application, based on Model-View-Controller pattern;
+ *
+ * @author Andrei Ciliboae
+ */
+public class AppService {
+
+}
---
+++ SpringHotel/src/main/java/ro/springhotel/hotel/service/ClientService.java
@@ -0,0 +1,12 @@
+package ro.springhotel.hotel.service;
+
+/**
+ * ClientService is the class that holds all the services used only by the Client, which
+ * allow the application to call methods specific to the Client, based on Model-View-Controller
+ * pattern;
+ *
+ * @author Andrei Ciliboae
+ */
+public class ClientService {
+
+}
