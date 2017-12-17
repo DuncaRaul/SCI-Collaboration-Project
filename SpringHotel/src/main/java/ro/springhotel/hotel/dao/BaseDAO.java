9a46892ab227925a73992063c016b083c3040f49
DuncaRaul <rauldunca94@yahoo.com>
2017-12-17 23:11:12 +0200

    Some work i have done on the DAO

--- SpringHotel/src/main/java/ro/springhotel/hotel/dao/BaseDAO.java
+++ SpringHotel/src/main/java/ro/springhotel/hotel/dao/BaseDAO.java
@@ -1,4 +1,16 @@
 package ro.springhotel.hotel.dao;
 
-public interface BaseDAO {
+import ro.springhotel.hotel.domain.AbstractModel;
+
+import java.util.Collection;
+
+public interface BaseDAO <T extends AbstractModel> {
+
+    Collection<T> getAll();
+
+    T findById(Long id);
+
+    T update(T model);
+
+    boolean delete(T model);
 }
