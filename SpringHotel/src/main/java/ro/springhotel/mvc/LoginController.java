0abe738b24b7cae949fdc87c81ddcfc8f8645fe3
DuncaRaul <rauldunca94@yahoo.com>
2018-01-05 19:41:38 +0200

    Added the LoginController for the aplication

--- SpringHotel/src/main/java/ro/springhotel/mvc/LoginController.java
+++ SpringHotel/src/main/java/ro/springhotel/mvc/LoginController.java
@@ -1,4 +1,56 @@
 package ro.springhotel.mvc;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+import ro.springhotel.hotel.domain.Client;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class LoginController {
+
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView("auth/login");
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/acces-denied", method = RequestMethod.GET)
+    public ModelAndView accesDenied() {
+        ModelAndView modelAndView = new ModelAndView("auth/acces-denied");
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/onLogin")
+    public ModelAndView onLogin(String userName, String pass, HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView();
+        //use UserService to check the login
+        boolean loginWithSucces = true;
+        if (loginWithSucces) {
+            Client client = new Client();
+            client.setUserName(userName);
+
+            request.getSession().setAttribute("currentClient", client);
+            modelAndView.setView(new RedirectView("/client"));
+        }
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/logout")
+    public String onLogout(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "/";
+    }
+
+
+
 }
